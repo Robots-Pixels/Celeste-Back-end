ead80a8d3c4656cd6e10bbc3ce32b763e6952edc
Robots-Pixels <Robots-Pixels>
2025-03-23 21:43:22 +0100

    Backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import messageRoute from "./routes/message.route.js";
+import bookingRoute from "./routes/booking.route.js";
+
+dotenv.config();
+
+const app = new express();
+
+app.use(express.json());
+app.use(cors({origin: "*"}));
+app.use("/api/message/", messageRoute);
+app.use("/api/reservation/", bookingRoute);
+
+mongoose
+    .connect((process.env.MONGO))
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000...")
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
